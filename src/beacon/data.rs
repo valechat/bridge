a971ec446967353761f25420d2cb17808afda083
merlinfuchs <merlinfuchs2001@gmail.com>
2021-12-16 23:38:07 +0100

    minor

--- src/beacon/data.rs
+++ src/beacon/data.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+#[derive(Serialize)]
 pub struct RegisterBeaconCallbackRequestData {}
 
+#[derive(Deserialize)]
 pub struct RegisterBeaconCallbackResponseData {}
